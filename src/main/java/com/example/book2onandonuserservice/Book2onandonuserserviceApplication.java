479566a22d732b3ff7a508373e7895c804b062bc
EatsMyFault <hiwhdtjs@gmail.com>
2025-11-13 13:22:11 +0900

    first push

---
+++ src/main/java/com/example/book2onandonuserservice/Book2onandonuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.book2onandonuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book2onandonuserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Book2onandonuserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book2onandonuserservice/Book2onandonuserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book2onandonuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book2onandonuserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

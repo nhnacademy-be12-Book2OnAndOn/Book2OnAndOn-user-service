11168bfaad9b82dcac9a45829cfbf26e9f680b7e
EatsMyFault <hiwhdtjs@gmail.com>
2025-11-17 16:04:31 +0900

    Refactor/ Request에 @NoArgsConstructor 어노테이션, message 추가

--- src/main/java/com/example/book2onandonuserservice/addres/domain/dto/request/UserAddressCreateRequest.java
+++ src/main/java/com/example/book2onandonuserservice/addres/domain/dto/request/UserAddressCreateRequest.java
@@ -5,19 +5,23 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UserAddressCreateRequest {
     @Size(max = 20)
+    @NotNull(message = "주소 이름은 필수입니다.")
     private String userAddressName;
 
     @NotNull
-    @Size(min = 1, max = 100)
+    @Size(min = 1, max = 100, message = "주소는 1자 이상 100자 이내로 작성해주세요")
+    @NotNull(message = "주소는 필수입니다.")
     private String userAddress;
 
-    @Size(max = 100)
+    @Size(max = 100, message = "상세주소는 100자 이내로 작성해주세요")
     private String userAddressDetail;
 
 }
--- src/main/java/com/example/book2onandonuserservice/addres/domain/dto/request/UserAddressUpdateRequest.java
+++ src/main/java/com/example/book2onandonuserservice/addres/domain/dto/request/UserAddressUpdateRequest.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UserAddressUpdateRequest {
     @NotNull

e042efad900ba54cb150f4c185c98eac05d427ed
EatsMyFault <hiwhdtjs@gmail.com>
2025-11-19 19:21:36 +0900

    Refactor/Role COUPON_ADMIN 추가

--- src/main/java/com/example/book2onandonuserservice/user/domain/entity/Role.java
+++ src/main/java/com/example/book2onandonuserservice/user/domain/entity/Role.java
@@ -9,6 +9,7 @@ public enum Role {
     USER("ROLE_USER", "일반회원"),
     SUPER_ADMIN("ROLE_SUPER_ADMIN", "총괄 관리자"),
     BOOK_ADMIN("ROLE_BOOK_ADMIN", "도서 관리자"),
+    COUPON_ADMIN("ROLE_COUPON_ADMIN", "쿠폰 관리자"),
     ORDER_ADMIN("ROLE_ORDER_ADMIN", "주문 관리자"),
     MEMBER_ADMIN("ROLE_MEMBER_ADMIN", "회원 관리자");
     private final String key;

8b774458987a4c34e0858af6c51d39edaf9e72d5
EatsMyFault <hiwhdtjs@gmail.com>
2025-12-02 16:01:33 +0900

    Refector/ ApplicationTest

--- src/test/java/com/example/book2onandonuserservice/Book2onandonuserserviceApplicationTests.java
+++ src/test/java/com/example/book2onandonuserservice/Book2onandonuserserviceApplicationTests.java
@@ -1,50 +1,7 @@
 package com.example.book2onandonuserservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-
-        // 1. Config Server, Eureka 끄기
-        "spring.cloud.config.enabled=false",
-        "eureka.client.enabled=false",
-
-        // 2. H2 DB 사용
-        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-
-        // 3. Redis 더미 설정
-        "spring.data.redis.host=localhost",
-        "spring.data.redis.port=6379",
-        "spring.data.redis.password=",
-        "spring.data.redis.repositories.enabled=false",
-
-        // 4. Encryption 더미 키
-        "encryption.secret-key=12345678901234567890123456789012",
-
-        // 5. JWT
-        "jwt.secret-key=test-jwt-secret",
-        "jwt.access-token-validity=1800",
-        "jwt.refresh-token-validity=604800",
-
-        // 7. Mail 더미
-        "spring.mail.host=localhost",
-        "spring.mail.port=1025",
-        "spring.mail.username=test",
-        "spring.mail.password=test",
-
-        // 8. 스케줄러 끄기
-        "spring.task.scheduling.enabled=false"
-
-})
 class Book2onandonuserserviceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 }

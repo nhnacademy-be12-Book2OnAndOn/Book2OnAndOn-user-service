d4d8257a41ad6fa9a11fa7b3da3b9f627c11c2e2
EatsMyFault <hiwhdtjs@gmail.com>
2025-12-16 13:26:06 +0900

    bugfix: 웰컴쿠폰

--- src/main/java/com/example/book2onandonuserservice/global/config/RabbitConfig.java
+++ src/main/java/com/example/book2onandonuserservice/global/config/RabbitConfig.java
@@ -1,6 +1,8 @@
 package com.example.book2onandonuserservice.global.config;
 
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,12 @@ public class RabbitConfig {
     public static final String EXCHANGE = "book2.dev.user.exchange.test";
     public static final String ROUTING_KEY_WELCOME = "coupon.welcome.test";
     public static final String ROUTING_KEY_BIRTHDAY = "coupon.birthday"; //이거 추가
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+
+        return new Jackson2JsonMessageConverter();
+    }
 
     @Bean
     public DirectExchange exchange() {

4eae2e7f41e5a85c645f5c9a20b4a8cbd229ef39
EatsMyFault <hiwhdtjs@gmail.com>
2025-12-16 10:27:55 +0900

    bugfix: Reactive Redis

--- src/main/java/com/example/book2onandonuserservice/global/config/AppConfig.java
+++ src/main/java/com/example/book2onandonuserservice/global/config/AppConfig.java
@@ -3,11 +3,9 @@ package com.example.book2onandonuserservice.global.config;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.example.book2onandonuserservice.global.client")
 @EnableDiscoveryClient
-@EnableRedisRepositories(basePackages = "com.example.book2onandonuserservice.auth.repository")
 public class AppConfig {
 }
